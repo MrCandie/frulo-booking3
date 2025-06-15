283069cd0af467361937ec6a19c67ebe8f14ecd8
Candie <106674322+MrCandie@users.noreply.github.com>
2025-06-15 17:12:16 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2TdbAfCDBcvaeT_kG9I5zM8m1FMUsBrg",
-  authDomain: "frulo-ccf37.firebaseapp.com",
-  projectId: "frulo-ccf37",
-  storageBucket: "frulo-ccf37.firebasestorage.app",
-  messagingSenderId: "341968609556",
-  appId: "1:341968609556:web:eb8691b612d2f8d7ed2201",
-  measurementId: "G-8PCBBQY0DG",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
